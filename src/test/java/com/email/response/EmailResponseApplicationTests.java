7299e151d9953fb006cb5ffbb418476201aa3205
pn97coder <143820468+Bhushan3253@users.noreply.github.com>
2025-11-02 18:49:28 +0530

    Initial commit - Spring Boot backend

---
+++ src/test/java/com/email/response/EmailResponseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.email.response;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailResponseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
